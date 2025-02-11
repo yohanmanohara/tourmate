952eb9ba9369d619ecd64881309cfee510a523ac
yohanmanohara <warushayohan80@gmail.com>
2025-02-11 12:51:34 +0530

    #

--- lib/widgets/top_nav.dart
+++ lib/widgets/top_nav.dart
@@ -2,27 +2,103 @@ import 'package:flutter/material.dart';
 import '../constants/style.dart';
 import '../helpers/responsiveness.dart';
 
-// import 'custom_text.dart';
+import 'custom_text.dart';
 
 AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
     AppBar(
-     leading: !ResponsiveWidget.isSmallScreen(context)
-    ? ConstrainedBox(
-        constraints: const BoxConstraints(maxWidth: 56), // Limit width
-        child: Padding(
-          padding: const EdgeInsets.only(left: 16),
-          child: Image.asset(
-            "assets/icons/logo.png",
-            width: 28,
+      leading: !ResponsiveWidget.isSmallScreen(context)
+          ? Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Image.asset(
+                    "assets/icons/logo.png",
+                    width: 28,
+                  ),
+                ),
+              ],
+            )
+          : IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                key.currentState?.openDrawer();
+              }),
+      title: Row(
+        children: [
+          Visibility(
+              visible: !ResponsiveWidget.isSmallScreen(context),
+              child: const CustomText(
+                text: "TourMate",
+                color: lightGrey,
+                size: 20,
+                weight: FontWeight.bold,
+              )),
+          Expanded(child: Container()),
+          IconButton(
+              icon: const Icon(
+                Icons.settings,
+                color: dark,
+              ),
+              onPressed: () {}),
+          Stack(
+            children: [
+              IconButton(
+                  icon: Icon(
+                    Icons.notifications,
+                    color: dark.withOpacity(.7),
+                  ),
+                  onPressed: () {}),
+              Positioned(
+                top: 7,
+                right: 7,
+                child: Container(
+                  width: 12,
+                  height: 12,
+                  padding: const EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                      color: active,
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(color: light, width: 2)),
+                ),
+              )
+            ],
           ),
-        ),
-      )
-    : IconButton(
-        icon: const Icon(Icons.menu),
-        onPressed: () {
-          key.currentState?.openDrawer();
-        },
+          Container(
+            width: 1,
+            height: 22,
+            color: lightGrey,
+          ),
+          const SizedBox(
+            width: 24,
+          ),
+          const CustomText(
+            text: "Yohan Manohara",
+            color: lightGrey,
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                color: active.withOpacity(.5),
+                borderRadius: BorderRadius.circular(30)),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(30)),
+              padding: const EdgeInsets.all(2),
+              margin: const EdgeInsets.all(2),
+              child: const CircleAvatar(
+                backgroundColor: light,
+                child: Icon(
+                  Icons.person_outline,
+                  color: dark,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
-
+      iconTheme: const IconThemeData(color: dark),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
     );
-
