800a8c0de41a03a7de676242445962871a804bb9
yohanmanohara <warushayohan80@gmail.com>
2025-02-11 20:25:15 +0530

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:mobileappdev/widgets/side_menu.dart';
+
 import './screen/login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import '../services/firebase_options.dart';
 import './layout.dart';
+import '../controllers/menu_controller.dart' as menu_controller;
+import '../controllers/navigation_controller.dart';
+
+import 'package:get/get.dart';
+import 'routing/routes.dart';
 void main () async {
+   Get.put(menu_controller.MenuController());
+  Get.put(NavigationController());
   WidgetsFlutterBinding.ensureInitialized();
 await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
@@ -22,7 +31,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  SiteLayout(), // Load the screen here
+      home:  SideMenu(), // Load the screen here
     );
   }
 }
---
+++ lib/routing/router.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_web_dashboard/pages/clients/clients.dart';
+// import 'package:flutter_web_dashboard/pages/drivers/drivers.dart';
+// import 'package:flutter_web_dashboard/pages/overview/overview.dart';
+// import 'package:flutter_web_dashboard/routing/routes.dart';
+
+// Route<dynamic> generateRoute(RouteSettings settings) {
+//   switch (settings.name) {
+//     case overviewPageRoute:
+//       return _getPageRoute(const OverviewPage());
+//     case driversPageRoute:
+//       return _getPageRoute(const DriversPage());
+//     case clientsPageRoute:
+//       return _getPageRoute(const ClientsPage());
+//     default:
+//       return _getPageRoute(const OverviewPage());
+//   }
+// }
+
+// PageRoute _getPageRoute(Widget child) {
+//   return MaterialPageRoute(builder: (context) => child);
+// }
