33f87674a02957acc8936696b235ac8823deaec5
charukaonline <nuwanthakonline@gmail.com>
2025-02-17 09:19:00 +0530

    call ar map in overview

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -3,21 +3,20 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../routing/routes.dart';
 import '../pages/authentication/login.dart';
 import '../pages/AR/ar_call.dart';
+import '../pages/overview/overview.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case overviewPageRoute:
-      return _getPageRoute(Ar_call());
+      return _getPageRoute(Overview());
 
     case arPageRoute:
       return _getPageRoute(Ar_call());
 
     case clientsPageRoute:
       return _getPageRoute(Ar_call());
-
-  
 
     default:
       return _getPageRoute(Ar_call());
@@ -27,4 +26,3 @@ Route<dynamic> generateRoute(RouteSettings settings) {
 PageRoute _getPageRoute(Widget child) {
   return MaterialPageRoute(builder: (context) => child);
 }
-
